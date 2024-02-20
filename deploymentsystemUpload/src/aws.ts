a43f9a461d99c1ba67bfc76da851342ca9e5f5bc
Karan Doshi <73486482+doshikaran@users.noreply.github.com>
2024-02-20 00:13:34 -0800

    Update aws.ts

--- deploymentsystemUpload/src/aws.ts
+++ deploymentsystemUpload/src/aws.ts
@@ -2,9 +2,9 @@ import { S3 } from "aws-sdk";
 import fs from "fs";
 
 const s3 = new S3({
-    accessKeyId: "64c1cf3c53779b370a0df2f7edbcd1ee",
-    secretAccessKey: "2bd1a1ef7335622bceb39b5e0cad23c0f9bdbe93b57ee979ca0a42062a73d07b",
-    endpoint: "https://a0638756644cedebb06d73f0c9d15fdf.r2.cloudflarestorage.com"
+    accessKeyId: "",
+    secretAccessKey: "",
+    endpoint: ""
 })
 
 export const uploadFile = async (fileName: string, localFilePath: string) => {
