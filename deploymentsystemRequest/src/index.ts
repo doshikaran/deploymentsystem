14fc969aabee15b23b326856c86f198aa552b8dd
Karan Doshi <73486482+doshikaran@users.noreply.github.com>
2024-02-20 00:13:07 -0800

    Update index.ts

--- deploymentsystemRequest/src/index.ts
+++ deploymentsystemRequest/src/index.ts
@@ -2,16 +2,15 @@ import { S3 } from "aws-sdk";
 import express from "express";
 
 const s3 = new S3({
-  accessKeyId: "64c1cf3c53779b370a0df2f7edbcd1ee",
+  accessKeyId: "",
   secretAccessKey:
-    "2bd1a1ef7335622bceb39b5e0cad23c0f9bdbe93b57ee979ca0a42062a73d07b",
-  endpoint: "https://a0638756644cedebb06d73f0c9d15fdf.r2.cloudflarestorage.com",
+    "",
+  endpoint: "",
 });
 
 const app = express();
 
 app.get("/*", async (request, response) => {
-  // id.100xdevs.com
   const host = request.hostname;
   const id = host.split(".")[0];
   const filePath = request.path;
